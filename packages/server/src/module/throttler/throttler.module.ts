e018eb938f8e06f5ef680b43c86476a0e2643bcb
Sigit Kuncoro <sigit.kunc@gmail.com>
2023-07-11 17:29:37 +0700

    feat: implement rate limit

--- packages/server/src/module/throttler/throttler.module.ts
+++ packages/server/src/module/throttler/throttler.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
@@ -12,6 +13,12 @@ import { ThrottlerModule } from '@nestjs/throttler';
         limit: config.get('THROTTLE_LIMIT'),
       }),
     }),
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
   ],
 })
 export class ThrottlerAsyncModule {}
