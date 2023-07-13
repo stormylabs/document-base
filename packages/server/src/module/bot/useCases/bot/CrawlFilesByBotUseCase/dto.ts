d3401ae50003f24a9a709c993452a4334d569541
Sigit Kuncoro <sigit.kunc@gmail.com>
2023-07-13 17:30:48 +0700

    feat(crawlbot): complete crawl file bot feature

---
+++ packages/server/src/module/bot/useCases/bot/CrawlFilesByBotUseCase/dto.ts
@@ -0,0 +1,43 @@
+import { DOCUMENT_LIMIT } from '@/shared/constants';
+import { JobStatus } from '@/shared/interfaces';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, Max, Min } from 'class-validator';
+
+export default class CrawlFilesByBotFileDTO {
+  @ApiProperty({
+    description: 'Max number of URLs to crawl',
+    minimum: 1,
+    maximum: DOCUMENT_LIMIT,
+    required: true,
+    type: Number,
+  })
+  @IsNumber()
+  @Min(1)
+  @Max(DOCUMENT_LIMIT)
+  limit: number;
+
+  @ApiProperty({
+    type: 'array', // 👈  array of files
+    items: {
+      type: 'string',
+      format: 'binary',
+    },
+    required: false,
+    description: 'Files of the bot',
+  })
+  files: Array<Express.Multer.File>;
+}
+
+export class CrawlFilesByBotResponseDTO {
+  @ApiProperty({
+    description: 'Crawl Files Job ID',
+    type: String,
+  })
+  jobId: string;
+
+  @ApiProperty({
+    description: 'Crawl Files Job Status',
+    enum: JobStatus,
+  })
+  status: JobStatus;
+}
---
+++ packages/server/src/module/bot/useCases/bot/CrawlFilesByBotUseCase/index.ts
@@ -0,0 +1,96 @@
+import { CrawlJobService } from '@/module/bot/services/crawlJob.service';
+import { DocIndexJobService } from '@/module/bot/services/docIndexJob.service';
+import { FilesService } from '@/module/files/services/files.service';
+import { Injectable, Logger } from '@nestjs/common';
+import UnexpectedError, {
+  BotNotFoundError,
+  InvalidInputError,
+  UnfinishedCrawlJobsError,
+  UnfinishedDocIndexJobsError,
+} from 'src/shared/core/AppError';
+import { Either, Result, left, right } from 'src/shared/core/Result';
+import { BotService } from '../../../services/bot.service';
+import CreateCrawlJobUseCase from '../../jobs/CreateCrawlJob';
+import CreateBotUseCase from '../CreateBot';
+import { CrawlFilesByBotResponseDTO } from './dto';
+
+type Response = Either<UnexpectedError, Result<CrawlFilesByBotResponseDTO>>;
+
+@Injectable()
+export default class CrawlFilesByBotUseCase {
+  private readonly logger = new Logger(CrawlFilesByBotUseCase.name);
+  constructor(
+    private readonly botService: BotService,
+    private readonly fileService: FilesService,
+    private readonly crawlJobService: CrawlJobService,
+    private readonly docIndexJobService: DocIndexJobService,
+    private readonly createBotUseCase: CreateBotUseCase,
+    private readonly createCrawlJobUseCase: CreateCrawlJobUseCase,
+  ) {}
+  public async exec(
+    botId: string,
+    files: Array<Express.Multer.File>,
+    limit: number,
+  ): Promise<Response> {
+    try {
+      this.logger.log(`Start creating bot with file`);
+
+      if (files.length < 0) {
+        return left(new InvalidInputError('files is required'));
+      }
+
+      this.logger.log(`Start uploading files into s3 buckets`);
+      const uploadFileResults = await Promise.all([
+        ...files.map(async (file) => {
+          return this.fileService.uploadFile(file.buffer, file.filename);
+        }),
+      ]);
+
+      const bot = await this.botService.findById(botId);
+      if (!bot) {
+        return left(new BotNotFoundError());
+      }
+
+      const unfinishedCrawlJobs = await this.crawlJobService.findUnfinishedJobs(
+        botId,
+      );
+
+      const unfinishedDocIndexJobs =
+        await this.docIndexJobService.findUnfinishedJobs(botId);
+
+      if (unfinishedCrawlJobs.length > 0) {
+        return left(
+          new UnfinishedCrawlJobsError(
+            unfinishedCrawlJobs.map((job) => job._id),
+          ),
+        );
+      }
+
+      if (unfinishedDocIndexJobs.length > 0) {
+        return left(
+          new UnfinishedDocIndexJobsError(
+            unfinishedDocIndexJobs.map((job) => job._id),
+          ),
+        );
+      }
+
+      // remove all documents before start crawling
+      await this.botService.removeAllDocuments(botId);
+      this.logger.log(`Removed all documents of bot ${botId}`);
+
+      this.logger.log('Start create crawler files jobs');
+      const urls = uploadFileResults.map((file) => file.url);
+
+      const result = await this.createCrawlJobUseCase.exec(botId, urls, limit);
+
+      if (result.isLeft()) {
+        return left(result.value);
+      }
+
+      this.logger.log(`Files are crawled by bot successfully`);
+      return right(Result.ok({ ...result.value.getValue() }));
+    } catch (err) {
+      return left(new UnexpectedError(err));
+    }
+  }
+}
