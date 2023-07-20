5ea73677a2b69f299ef6fe1621b780b0f2ef05e6
Sigit Kuncoro <sigit.kunc@gmail.com>
2023-07-20 13:34:59 +0700

    build(nx): integrate nx to api package and remo unused code

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,22 +1,39 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-import { AppModule } from './app/app.module';
+import { AppModule } from './app.module';
+import { NormalizeQueryParamsValidationPipe } from './shared/NormalizeQueryParamsValidationPipe';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(
+    new NormalizeQueryParamsValidationPipe(),
+    new ValidationPipe({
+      transform: true,
+    }),
   );
-}
 
+  app.use(cookieParser());
+  app.enableCors();
+  const config = app.get(ConfigService);
+
+  const NODE_ENV = config.get<string>('NODE_ENV');
+
+  if (NODE_ENV !== 'production') {
+    const options = new DocumentBuilder()
+      .setTitle('DocumentBase API')
+      .setDescription('API documentations of Document Base')
+      .setVersion('1.0')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api/v1/docs', app, document);
+  }
+
+  await app.listen(3000);
+}
 bootstrap();
