8fc18e420bb594f6639dacc74e417595f8e099cf
Sigit Kuncoro <sigit.kunc@gmail.com>
2023-07-20 13:44:57 +0700

    build(nx): change name api to server

--- packages/server/jest.config.ts
+++ packages/server/jest.config.ts
@@ -7,5 +7,5 @@ export default {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/packages/api',
+  coverageDirectory: '../../coverage/packages/server',
 };
