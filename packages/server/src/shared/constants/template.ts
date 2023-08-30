9776f89a1c7d9ff2946a2d05b618f650b60d864e
siddharth <me@siddharthjain.in>
2023-08-30 09:47:05 +0530

    changed prompt

--- packages/server/src/shared/constants/template.ts
+++ packages/server/src/shared/constants/template.ts
@@ -6,19 +6,28 @@ const templates = {
   =========
   CONTEXT: {context}
   =========
-  The FINAL ANSWER must always be styled using markdown.
-  Use bullet points, lists, paragraphs and text styling to present the answer in markdown.
-  Answer the QUESTION based on the CONTEXT provided
-  Do not repeat the QUESTION in the FINAL ANSWER.
-  If the QUESTION is a small-talk, answer it with a small-talk response without basing on the CONTEXT.
-  Please make sure that the FINAL ANSWER is within 5 sentences.
-  Do not include any information irrelevant to the QUESTION in the FINAL ANSWER.
 
   The question is provided between the * lines:
   **********
   QUESTION: {question}
   **********
+
+  The FINAL ANSWER must always be styled using markdown.
+  Use bullet points, lists, paragraphs and text styling to present the answer in markdown.
+  
+  Answer the QUESTION based only on the CONTEXT provided.
+  Do not use any information outside of the CONTEXT to answer the QUESTION.
+  If you are not sure about the answer, reply with the Fallback Message or "I don't know".
+
+  Do not repeat the QUESTION in the FINAL ANSWER.
+
+  If the QUESTION is a small-talk, answer it with a small-talk response without basing on the CONTEXT.
+  Please make sure that the FINAL ANSWER is within 5 sentences.
+  Keep the FINAL ANSWER concise and to the point.
+
   The FINAL ANSWER should be in the same language as the QUESTION.
+
+
 
   FINAL ANSWER:
  `,
@@ -31,28 +40,6 @@ const templates = {
   FOLLOW_UP_QUESTION: {question}
   Standalone question:`,
   summerierTemplate: `Summarize the following text. You should follow the following rules when generating and answer:`,
-  refinePromptTemplate: `
-  The original question is provided between the * lines:
-  ****************
-  {question}
-  ****************
-  The existing answer is provided below between the = lines: 
-  ================
-  {existing_answer}
-  ================
-  We have the opportunity to refine the existing answer  
-  (only if needed) with some more context below.  
-  The context is provided below between the - lines:
-  ------------
-  {context}
-  ------------
-  Answer the question precisely.
-  Do not repeat the question in the answer.
-  Do not make up any information.
-  You must provide a response, either original answer or refined answer.
-
-  
-  REFINED ANSWER:`,
 };
 
 export { templates };
