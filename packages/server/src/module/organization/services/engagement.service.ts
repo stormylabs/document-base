2edbc509b588f02abfa04169f09d182e05a71ec4
siddharth <me@siddharthjain.in>
2023-10-11 13:27:38 +0530

    error corection

--- packages/server/src/module/organization/services/engagement.service.ts
+++ packages/server/src/module/organization/services/engagement.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common';
-
-
 import { EngagementRepository } from '../repositories/engagement.repository';
 import { EngagementData } from '@/shared/interfaces/engagement';
 
