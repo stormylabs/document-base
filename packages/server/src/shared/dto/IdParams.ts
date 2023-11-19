cb7e20c92301f90bd66348a7130026f945063291
Sigit Kuncoro <sigit.kunc@gmail.com>
2023-11-19 23:07:15 +0700

    feat: add invalid mongoId validation

--- packages/server/src/shared/dto/IdParams.ts
+++ packages/server/src/shared/dto/IdParams.ts
@@ -1,8 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { IsMongoId, IsString } from 'class-validator';
+import { SafeMongoIdTransform } from '@/shared/utils/safeMongoIdTransform';
 
 export class IdParams {
-  @ApiProperty({ name: 'id', type: String })
+  @ApiProperty({
+    description: 'Id',
+    required: true,
+    type: String,
+    example: '61d9cfbf17ed7311c4b3e485',
+  })
+  @IsMongoId()
   @IsString()
+  @Transform((value) => SafeMongoIdTransform(value))
   id: string;
 }
