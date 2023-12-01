0285ee6f1a95afeffee7146fc6b952086503e2ce
Sigit Kuncoro <sigit.kunc@gmail.com>
2023-12-01 09:09:35 +0700

    feat(Dashboard):  init dashboard project

---
+++ packages/stormy-dashboard/app/page.tsx
@@ -0,0 +1,10 @@
+import styles from './page.module.scss';
+
+export default async function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.scss file.
+   */
+  return <div>Hello</div>;
+}
