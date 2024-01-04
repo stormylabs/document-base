e5aa60892c87876710e1ea2f9541bc6827ee267a
Sigit Kuncoro <sigit.kunc@gmail.com>
2024-01-05 01:32:23 +0700

    docs: jsdoc

--- packages/web-chatbot/services/bot.ts
+++ packages/web-chatbot/services/bot.ts
@@ -4,5 +4,11 @@ import httpClient from 'utils/httpClient';
 export const getBotInfo = async (botId: string) =>
   httpClient.get(`/bot/${botId}`);
 
+/**
+ *
+ * @param botId
+ * @param data
+ * @returns
+ */
 export const sendMessage = async (botId: string, data: SendMessage) =>
   httpClient.post(`/bot/message/${botId}`, data);
